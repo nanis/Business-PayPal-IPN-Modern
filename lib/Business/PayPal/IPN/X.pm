040dfb44292ed9f22c2e488204c0da5059c4ba45
A. Sinan Unur <nanis@cpan.org>
2012-08-28 15:47:08 -0400

    Add descriptions to exceptions

--- lib/Business/PayPal/IPN/X.pm
+++ lib/Business/PayPal/IPN/X.pm
@@ -13,42 +13,49 @@ use Exception::Class (
     $class,
 
     "${class}::Invalid" => {
-        isa => $class
+        isa => $class,
+        description => 'PayPal says the request we received is not valid',
     },
 
     "${class}::InvalidReceiverEmail" => {
         isa => $class,
         fields => [ qw( got expected )],
+        description => 'Receiver email in verified PayPal request does not match what we were told to expect in "my_email"',
     },
 
     "${class}::Connection" => {
         isa => $class,
         fields => [qw( ua_error )],
+        description => 'Connection problem during request verification',
     },
 
     "${class}::FailedReadRequest" => {
         isa => $class,
+        decription => 'Failed to read request from PayPal',
     },
 
     "${class}::RequestTooBig" => {
         isa => $class,
         fields => [qw( limit )],
+        description => 'The request from PayPal is larger than we are willing to accept',
     },
 
     "${class}::MissingContentLength" => {
         isa => $class,
+        decription => '$ENV{CONTENT_LENGTH} is undef',
     },
 
     "${class}::FailedReadQuery" => {
         isa => $class,
         fields => [qw( os_error )],
+        description => 'Failed to read query',
     },
 
     "${class}::FailedVerifyRequest" => {
         isa => $class,
+        description => 'Request to verify information received failed',
     },
 );
-
 
 
 1;
@@ -65,6 +72,9 @@ Business::PayPal::IPN::Modern::X - Exceptions for Business::PayPal::Modern::IPN
 
 =head1 SYNOPSIS
 
+    Business::PayPal::IPN::Modern::X::FailedReadRequest->throw(
+        error => '...',
+    );
 
 =head1 AUTHOR
 
